11ecff898e8f15ca3e0b3f9c3dd3984643d8a4c1
housenkui <housenkui@gmail.com>
2020-03-14 11:31:56 +0800

    Init project.

---
+++ WebViewJavascriptBridge_swift/WebViewJavascriptBridge_swift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WebViewJavascriptBridge_swift
+//
+//  Created by 侯森魁 on 2020/3/14.
+//  Copyright © 2020 侯森魁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
